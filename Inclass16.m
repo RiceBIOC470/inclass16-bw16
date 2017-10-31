69dad263488eef2cbde5a9a008e60a3cd60e14d4
bw16 <31257600+bw16@users.noreply.github.com>
2017-10-31 14:28:18 -0500

    Add files via upload

--- Inclass16.m
+++ Inclass16.m
@@ -28,10 +28,51 @@
 % like and fill the peaks cell array as described above so that each of the two cells 
 % has 6 column matrix with x,y,area,-1,chan1 intensity, chan 2 intensity
 
+reader=bfGetReader('nfkb_movie1.tif');
+iplane1=reader.getIndex(0,0,0) + 1;
+img1=bfGetPlane(reader,iplane1);
+iplane2=reader.getIndex(0,0,1)+1;
+img2=bfGetPlane(reader,iplane2);
+
+imshow(img1,[100 2000]);
+imshow(img2,[100 2000]);
+
+imshowpair(imadjust(img1), imadjust(img2));
+
+mask1=img1>800;
+mask1=imclose(mask1,strel('disk',6));
+mask1=imopen(mask1, strel('disk', 3));
+
+mask2=img2>800;
+mask2=imclose(mask2, strel('disk', 6));
+mask2=imopen(mask2, strel('disk', 3));
+imshowpair(mask1,mask2);
+
+stats1=regionprops(mask1,img1,'Centroid','Area','MeanIntensity');
+stats2=regionprops(mask2,img2,'Centroid','Area','MeanIntensity');
+
+xy1=cat(1,stats1.Centroid);
+a1=cat(1,stats1.Area);
+mi1=cat(1,stats1.MeanIntensity);
+tmp=-1*ones(size(a1));
+peaks{1}=[xy1,a1,-1,tmp,mi1];
+
+xy2=cat(1,stats2.Centroid);
+a2=cat(1,stats2.Area);
+mi2=cat(1,stats2.MeanIntensity);
+tmp=-1*ones(size(a2));
+peaks{2}=[xy2,a2,-1,tmp,mi2];
 % Part 2. Run match frames on this peaks array. ensure that it has filled
 % the entries in peaks as described above. 
+
+peaks_matched=MatchFrames(peaks,1,50);
 
 % Part 3. Display the image from the second frame. For each cell that was
 % matched, plot its position in frame 2 with a blue square, its position in
 % frame 1 with a red star, and connect these two with a green line. 
 
+figure; 
+imshow(img2,[100 2000]); 
+hold on;
+plot(peaks{1}(:,1),peaks{1}(:,2), 'r*', 'MarkerSize',5)
+plot(peaks{2}(:,1),peaks{2}(:,2), 'cs', 'MarkerSize',5)
