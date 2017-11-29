369f8e77844cad10d8b11fad874be2a3c2a35368
gbri140 <glb3@rice.edu>
2017-11-28 21:26:35 -0600

    Update Inclass16.m

--- Inclass16.m
+++ Inclass16.m
@@ -1,4 +1,10 @@
 % Inclass16
+%GB comments
+1 100 
+2 90 Matchframes should be entered as MatchedFrames(peaks,2, 50); 
+3 75 This should really be looped for all possible matching cells between timeframes
+overall 88
+
 
 %The folder in this repository contains code implementing a Tracking
 %algorithm to match cells (or anything else) between successive frames. 
